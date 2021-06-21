b0e63c3ced38b8707c2890af9ae64e92211f7530
luckyluckydadada <savoki1024@gmail.com>
2021-06-21 22:48:21 +0800

    update

--- README.md
+++ README.md
@@ -25,10 +25,25 @@ roslaunch livox_color color_livox_rviz.launch
 ```
 rosbag record /livox/color_lidar
 ```
+## 将录制的点云rosbag(PointCloud2)转为pcd(xyz)，并合并
+```
+source ./devel/setup.bash 
+roslaunch livox_color bag2merge_pcd.launch \
+input:=/home/$USER/rosbag/livox/PointCloud2bag/2021-06-11-15-10-57.bag \
+output:=/home/$USER/rosbag/livox/PointCloud2bag/ \
+threshold:=1
+参数：
+    input: bag的路径
+    output： pcd的路径
+    threshold： 帧数，3代表1s，目前彩色点云处理速率是1秒3帧；1代表不合并;0代表合并所有帧
+解释：
+    将11.bag(11代表这个bag文件含点云11帧)输出为3.pcd（3代表3帧合并一个pcd，既1秒）
+
+```
 ## 将录制的彩色点云rosbag(PointCloud2)转为pcd(xyzrgb)，并合并
 ```
 source ./devel/setup.bash 
-roslaunch livox_color colorbag2pcd.launch \
+roslaunch livox_color colorbag2merge_pcd.launch \
 input:=/home/$USER/rosbag/livox/colorbag/fixed_scenes1/21.bag \
 output:=/home/$USER/rosbag/livox/colorbag/fixed_scenes1/ \
 threshold:=1
---
+++ bag2merge_pcd.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/io/pcd_io.h>
+
+#include <ros/ros.h>
+#include <std_msgs/Header.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+
+using namespace std;
+
+pcl::PointCloud<pcl::PointXYZI> livox_cloud_single_frame,livox_cloud; 
+string input_bag_path, output_path;
+int threshold_lidar;
+
+void loadAndSavePointcloud();
+
+void loadAndSavePointcloud() {
+    string path = input_bag_path;
+    fstream file_;
+    file_.open(path, ios::in);
+    if (!file_) {
+        cout << "File " << path << " does not exit" << endl;
+        return;
+    }
+    ROS_INFO("Start to load the rosbag %s", path.c_str());
+    rosbag::Bag bag;
+    try {
+        bag.open(path, rosbag::bagmode::Read);
+    } catch (rosbag::BagException e) {
+        ROS_ERROR_STREAM("LOADING BAG FAILED: " << e.what());
+        return;
+    }
+
+    vector<string> types;
+    types.push_back(string("sensor_msgs/PointCloud2")); 
+    rosbag::View view(bag, rosbag::TypeQuery(types));
+
+    int cloudCount = 0;
+    for (const rosbag::MessageInstance& m : view) {
+        pcl::fromROSMsg(*(m.instantiate<sensor_msgs::PointCloud2 >()), livox_cloud_single_frame);
+        if (0 == cloudCount){
+            livox_cloud = livox_cloud_single_frame;
+        }
+        else{
+            livox_cloud += livox_cloud_single_frame;
+        }
+        pcl::io::savePCDFileASCII(output_path, livox_cloud);
+        // std::cout<< livox_cloud.point_num<<std::endl; // mid40：一帧10000个点；1秒10帧率
+        ++cloudCount;
+        if (threshold_lidar == 0){
+            continue;
+        }
+        if (cloudCount >= threshold_lidar) {
+            break;
+        }
+    }
+}
+
+void getParameters() {
+    cout << "Get the parameters from the launch file" << endl;
+
+    if (!ros::param::get("input_bag_path", input_bag_path)) {
+        cout << "Can not get the value of input_bag_path" << endl;
+        exit(1);
+    }
+    else {
+        cout << input_bag_path << endl;
+    }
+    if (!ros::param::get("output_pcd_path", output_path)) {
+        cout << "Can not get the value of output_path" << endl;
+        exit(1);
+    }
+    if (!ros::param::get("threshold_lidar", threshold_lidar)) {
+        cout << "Can not get the value of threshold_lidar" << endl;
+        exit(1);
+    }
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "colorbag2pcd");
+    getParameters();
+    loadAndSavePointcloud();
+    ROS_INFO("Finish!");
+    return 0;
+}
+
